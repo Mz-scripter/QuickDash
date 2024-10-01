0e92fca6b5a4ccbc18ea09e61971c9554d9483fe
Adeko Muheez <mzscripterx5@gmail.com>
2024-10-01 07:38:50 +0100

    Each user Cart items is now seperate

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from sqlalchemy.orm import relationship
 from functools import wraps
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "LXS2U[j&'iMg<)5R~@!Q%0TKn"
@@ -23,15 +24,16 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Users.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 # Configure Tables
-class Users(UserMixin, db.Model):
-    __tablename__ = "users"
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), nullable=False, unique=True)
     password = db.Column(db.String(250), nullable=False)
+    cart_items = db.relationship('Cart', backref='user', lazy=True)
 
 class Items(db.Model):
     __tablename__ = 'items'
@@ -44,6 +46,7 @@ class Items(db.Model):
 class Cart(db.Model):
     __tablename__ = 'cart'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     dish = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.String(10), nullable=False)
     time = db.Column(db.String(250), nullable=False)
@@ -51,22 +54,33 @@ class Cart(db.Model):
 
 # with app.app_context():
 #     db.create_all()
+
 @app.before_request
 def set_variable():
-    g.cart_num = calculate_num_cart()
+    if current_user.is_authenticated:
+        g.cart_num = calculate_num_cart()
+    else:
+        g.cart_num = 0
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     items = Items.query.all()
+    random.shuffle(items)
     return render_template('index.html', all_items=items, numc=g.cart_num)
+
+@app.route('/header', methods=["GET", "POST"])
+def header():
+    return render_template('header.html', numc=g.cart_num)
 
 @app.route('/add-to-cart', methods=["GET", "POST"])
 @login_required
 def add_to_cart():
     item_id = request.args.get('id')
     with app.app_context():
+        user_id = User.query.get(current_user.id)
         item_to_add = db.session().query(Items).get(item_id)
         cart_item = Cart(
+            user_id = user_id.id,
             dish = item_to_add.dish,
             rating = item_to_add.rating,
             time = item_to_add.time,
@@ -80,13 +94,13 @@ def add_to_cart():
 def register():
     form = RegisterForm()
     if request.method == "POST":
-        if Users.query.filter_by(email=form.email.data).first():
+        if User.query.filter_by(email=form.email.data).first():
             flash("You've already registered with that email, login instead", 'error')
             return redirect(url_for('login'))
         else:
             with app.app_context():
                 hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-                new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
+                new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user, remember=True)
@@ -99,7 +113,7 @@ def login():
     form = LoginForm()
     if request.method == "POST":
         with app.app_context():
-            user = Users.query.filter_by(email=form.email.data).first()
+            user = User.query.filter_by(email=form.email.data).first()
             if not user:
                 flash("Email doesn't exist. Register instead", "error")
                 return redirect(url_for('register'))
@@ -113,6 +127,7 @@ def login():
     return render_template('login.html', form=form)
 
 @app.route('/add-item', methods=['GET', 'POST'])
+@login_required
 def add_item():
     form = AddItemForm()
     if request.method == "POST":
@@ -126,7 +141,7 @@ def add_item():
             db.session.add(new_item)
             db.session.commit()
             return redirect(url_for('home'))
-    return render_template('add-item.html', form=form)
+    return render_template('add-item.html', form=form, numc=g.cart_num)
 
 @app.route('/logout', methods=["GET", "POST"])
 @login_required
@@ -134,9 +149,10 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+@login_required
 def calculate_num_cart():
-    cart = Cart.query.all()
-    return len(cart)
+    cart_items = Cart.query.filter_by(user_id=current_user.id).all()
+    return len(cart_items)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
