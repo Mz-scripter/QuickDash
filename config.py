0ad84aa9e50dfc3375b95a445d543e7e1af21d4b
Adeko Muheez <mzscripterx5@gmail.com>
2024-10-16 11:01:05 +0100

    Fixed the verification

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ class Config:
     SECRET_KEY = "LXS2U[j&'iMg<)5R~@!Q%0TKn"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///quickdash.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmai.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = 'adekomuheez567@gmail.com'
     MAIL_PASSWORD = 'bgxt uqqx ipsw avws'
+    MAIL_DEFAULT_SENDER = ('QuickDash', 'adekomuheez567@gmail.com')
     FLW_PUBLIC_KEY = "FLWPUBK_TEST-5de963cfabeed4edab5b8b6a2f5a6984-X"
     FLW_SECRET_KEY = "FLWSECK_TEST-10e26c3e22d3883f6258bd342016396b-X"
     FLW_REDIRECT_URL = "https://github.com/Mzed-io"
